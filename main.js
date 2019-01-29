b9c2a4eee53e262167a5c1a25c433e6b09aac34a
Talha AYDIN <talhaaydin58@outlook.com>
2019-01-30 00:33:42 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,60 @@
+var tipCalculatorForm = document.querySelector("#tipCalculatorForm");
+
+var billInput = document.querySelector("#bill");
+var serviceScore = document.querySelector("#serviceScore");
+var tipInput = document.querySelector("#tip");
+
+var resultField = document.querySelector("div.result");
+resultField.setAttribute("style", "display:none;");
+
+tipCalculatorForm.addEventListener("submit", (e) => {
+
+    clearForm();
+    
+    if(billInput.value === ""){ createAlertLabel("Boş geçilemez.", billInput) }
+    if(serviceScore.value === "0"){ createAlertLabel("Boş geçilemez.", serviceScore) }
+    if(tipInput.value === ""){ createAlertLabel("Boş geçilemez.", tipInput) }
+
+
+    // Calculator tip
+    var result = ((billInput.value*serviceScore.value)/100)/tipInput.value;    
+    
+    // Control div.result span
+    checkResultSpan();   
+
+    resultField.setAttribute("style", "display:block;");
+
+    // create div.result span element with result
+    createResultSpan(result);
+    
+    
+
+    e.preventDefault();
+});
+
+function clearForm(){
+    var alertLabel = document.querySelectorAll(".alert-label");
+    
+    alertLabel.forEach(function(label){
+        label.remove();
+    });
+}
+function checkResultSpan(){
+    var isThereResultSpan = document.querySelector(".result span");
+    if(isThereResultSpan){
+        isThereResultSpan.remove();
+    }
+}
+function createResultSpan(result){
+    var resultSpan = document.createElement("span");
+    resultSpan.appendChild(document.createTextNode(`${result} TL`));
+    
+    resultField.appendChild(resultSpan);
+}
+function createAlertLabel(alert, child){
+    var alertLabel = document.createElement("label");
+    alertLabel.className = "alert-label";
+    alertLabel.textContent = alert;
+
+    child.parentElement.appendChild(alertLabel);
+}
